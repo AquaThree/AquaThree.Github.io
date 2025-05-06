7dc0042b335f54111397c524ff31de2d89e65d70
Aqua3 <2523211196@qq.com>
2025-05-06 23:33:25 +0800

    Update Files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+this.onLoad();
+
 function onLoad() {
 }
 
@@ -20,12 +22,20 @@ function clickGoLearnMoreButton() {
     });
 }
 
-function clickOpenPopupButton() {
+function clickOpenPopupButton(index) {
+    var popup = document.getElementById("popup").children[index];
+    if(index == 0) {
+        popup.style.width = "400px";
+        popup.style.height = "225px";
+    } else if (index == 1) {
+        popup.style.width = "350px";
+        popup.style.height = "150px";
+    }
+    popup.style.display = "flex";
     document.getElementById("popup").style.display = "flex";
 }
 
-function clickClosePopupButton() {
+function clickClosePopupButton(index) {
     document.getElementById("popup").style.display = "none";
+    document.getElementById("popup").children[index].style.display = "none";
 }
-
-onLoad();
