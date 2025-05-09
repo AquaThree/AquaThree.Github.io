d3c680654a37ac1549deb2807dbac6cf76f7ad8d
Aqua3 <2523211196@qq.com>
2025-05-09 14:58:29 +0800

    Update Files

--- js/main.js
+++ js/main.js
@@ -25,10 +25,18 @@ function clickGoLearnMoreButton() {
 function clickOpenPopupButton(index) {
     var popup = document.getElementById("popup").children[index];
     if(index == 0) {
-        popup.style.width = "400px";
+        if(window.screen.width >= 400) {
+           popup.style.width = "400px";
+        } else {
+            popup.style.width = window.screen.width + "px";
+        }
         popup.style.height = "225px";
     } else if (index == 1) {
-        popup.style.width = "350px";
+        if(window.screen.width >= 350) {
+           popup.style.width = "350px";
+        } else {
+            popup.style.width = window.screen.width + "px";
+        }
         popup.style.height = "150px";
     }
     popup.style.display = "flex";
